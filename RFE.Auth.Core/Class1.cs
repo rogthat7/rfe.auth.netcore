e9fbdfa8b32240b7187d78653ca75164f8f917f7
rogthat7 <rogthat7@gmail.com>
2021-10-15 20:41:45 +0530

    Added Project Structure

---
+++ RFE.Auth.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RFE.Auth.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ RFE.Auth.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RFE.Auth.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
